0a32d381abc61ec58743116edee7c57c1aa89e65
Caleb <calebfrances@msn.com>
2020-02-12 21:52:02 -0600

    Method created and switch created

--- Game.cs
+++ Game.cs
@@ -2,8 +2,23 @@ using System;
 
 namespace ConsoleGame
 {
-  class Game : SuperGame
-  {
-    
-  }
+    class Game : SuperGame
+    {
+        public new static UpdatePosition(string key1, out int x, out int y)
+        {
+            switch (key1)
+            {
+                case "LeftArrow":
+                    x--;
+                case "RightArrow":
+                    x++;
+                case "UpArrow":
+                    y--;
+                case "DownArrow":
+                    y++;
+            }
+
+
+        }
+    }
 }
