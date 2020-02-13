1f79ee5060a443d82976bca0c71d78cb7cbb4dde
Caleb <calebfrances@msn.com>
2020-02-12 22:04:29 -0600

    UpdateCursor method and switch case. KeepInBounds Method

--- Game.cs
+++ Game.cs
@@ -4,20 +4,49 @@ namespace ConsoleGame
 {
     class Game : SuperGame
     {
-        public new static UpdatePosition(string key1, out int x, out int y)
+        public new static void UpdatePosition(string key1, out int x, out int y)
         {
             switch (key1)
             {
                 case "LeftArrow":
                     x--;
+                    break;
                 case "RightArrow":
                     x++;
+                    break;
                 case "UpArrow":
                     y--;
+                    break;
                 case "DownArrow":
                     y++;
+                    break;
+                default:
+                    break;
             }
+        }
+        public new static char UpdateCursor(string input)
+        {
+            switch (input)
+            {
+                case "LeftArrow":
+                    '<';
+                    break;
+                case "UpArrow":
+                    '^';
+                    break;
+                case "RightArrow":
+                    '>';
+                    break;
+                case "DownArrow":
+                    'v';
+                    break;
+                default:
+                    break;
 
+            }
+        }
+        public new static int KeepInBounds(int cood, int max)
+        {
 
         }
     }
